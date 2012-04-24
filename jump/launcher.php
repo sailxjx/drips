c91a8f65c50be3776decc9b651d0c46f6d3dafeb
sailxjx <sailxjx@163.com>
2012-04-24 19:00:35 +0800

    daily sync: 2012年04月24日19:00:01

--- jump/launcher.php
+++ jump/launcher.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
 define('APP_PATH', realpath(dirname(__FILE__)) . '/'); //工作目录
+define('ENV', 'dev'); //设置工作环境:dev/ga
 require APP_PATH . 'gfuncs.php'; //全局方法
 require APP_PATH . 'classes/Core.php';
 JobCore::getIns()->init($argv);
