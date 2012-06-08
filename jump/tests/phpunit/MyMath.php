00b9ec64fd4b05f7ac377a560750219af76c8610
sailxjx <sailxjx@163.com>
2012-06-08 19:00:30 +0800

    daily sync: 2012年06月08日19:00:01

---
+++ jump/tests/phpunit/MyMath.php
@@ -0,0 +1,13 @@
+<?php
+class MyMath{
+
+    /**
+     * @assert (1,3) == 4
+     * @assert (2,2) == 4
+     * @assert (2,3) == 4
+     */
+    public function add($a, $b) {
+        return $a+$b;
+    }
+
+}
