42f1ac3081f6b484621fc2838086f1cfd53f1dc1
sailxjx <sailxjx@163.com>
2013-04-02 17:25:51 +0800

    change variable name

--- dysort/src/dysort.rb
+++ dysort/src/dysort.rb
@@ -1,23 +1,23 @@
 module Dysort
-  def stepIn(dataList)
-    minIdx = 0  # minimal index
-    maxIdx = 0  # maximal index
+  def stepIn(list)
+    min = 0  # minimal index
+    max = 0  # maximal index
     differ = 0  # max differ
-    minIdxTmp = nil  # temp minimal index
-    for i in 1...dataList.length
-      if dataList[i] > dataList[maxIdx]  # replace the maximal index
-        maxIdx = i  # new maximal index
-        differ = dataList[i] - dataList[minIdx]  # new differ
-      elsif dataList[i] < dataList[minIdx] and ( minIdxTmp == nil or dataList[i] < dataList[minIdxTmp] )  # replace the temp minimal index
-        minIdxTmp = i  # change temp minimal index
-      elsif minIdxTmp != nil and dataList[i] - dataList[minIdxTmp] > differ  # if current index minus temp minimal index is bigger than differ, replace it
-        differ = dataList[i] - dataList[minIdxTmp]  # new differ
-        minIdx = minIdxTmp  # new minimal index
-        maxIdx = i  # new maximal index
+    minTmp = nil  # temp minimal index
+    for i in 1...list.length
+      if list[i] > list[max]  # replace the maximal index
+        max = i  # new maximal index
+        differ = list[i] - list[min]  # new differ
+      elsif list[i] < list[min] and ( minTmp == nil or list[i] < list[minTmp] )  # replace the temp minimal index
+        minTmp = i  # change temp minimal index
+      elsif minTmp != nil and list[i] - list[minTmp] > differ  # if current index minus temp minimal index is bigger than differ, replace it
+        differ = list[i] - list[minTmp]  # new differ
+        min = minTmp  # new minimal index
+        max = i  # new maximal index
       else
         next
       end
     end
-    return [dataList[minIdx], dataList[maxIdx]]
+    return [list[min], list[max]]
   end
 end
